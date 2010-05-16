6f720b8b85873feabb4f58da3f9b877520c73ace
Karl Roos <karlroos93@gmail.com>
2010-05-16 12:20:07 +0200

    Added Solidocs_I18n::date(), time(), date_time()

--- System/Packages/Solidocs/I18n.php
+++ System/Packages/Solidocs/I18n.php
@@ -1,3 +1,43 @@
 <?php
 class Solidocs_I18n
-{}
+{
+	/**
+	 * Date format
+	 */
+	public $date_format = 'Y-m-d';
+	
+	/**
+	 * Time format
+	 */
+	public $time_format = 'H:i:is';
+	
+	/**
+	 * Date
+	 *
+	 * @param integer
+	 * @return string
+	 */
+	public function date($time){
+		return date($this->date_format, $time);
+	}
+	
+	/**
+	 * Time
+	 *
+	 * @param integer
+	 * @return string
+	 */
+	public function time($time){
+		return date($this->time_format, $time);
+	}
+	
+	/**
+	 * Date time
+	 *
+	 * @param integer
+	 * @return string
+	 */
+	public function date_time($time){
+		return $this->date($time) . ' ' . $this->time($time);
+	}
+}
