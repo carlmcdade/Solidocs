7633d82dfe279454175b3adffb1feccdb2ffe9e3
Karl Roos <karlroos93@gmail.com>
2010-06-03 20:42:50 +0200

    Added Solidocs_Model_User::has_access()

--- System/Packages/Solidocs/Model/User.php
+++ System/Packages/Solidocs/Model/User.php
@@ -23,4 +23,22 @@ class Solidocs_Model_User extends Solidocs_Base
 	public function has_session(){
 		return !is_null($this->user);
 	}
+	
+	/**
+	 * Has access
+	 *
+	 * @param integer
+	 * @return bool
+	 */
+	public function has_access($level, $exact = false){
+		if(!isset($this->user->access_level)){
+			return false;
+		}
+		
+		if($exact){
+			return ($this->user->access_level == $level);
+		}
+		
+		return ($this->user->access_level >= $level);
+	}
 }
