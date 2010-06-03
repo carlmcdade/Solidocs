bf62a771c7f3e53f678ff6d8a2ab3723cd317cb2
Karl Roos <karlroos93@gmail.com>
2010-06-03 20:53:47 +0200

    Added Solidocs_Controller_Action::set_access(), forward(), do_404().

--- System/Packages/Solidocs/Controller/Action.php
+++ System/Packages/Solidocs/Controller/Action.php
@@ -2,6 +2,25 @@
 class Solidocs_Controller_Action extends Solidocs_Controller
 {
 	/**
+	 * Acl
+	 */
+	public $acl;
+	
+	/**
+	 * Set access
+	 *
+	 * @param string
+	 * @param integer
+	 * @param string|bool
+	 */
+	public function set_access($action, $level, $do = false){
+		$this->acl[$action] = array(
+			'level'	=> $level,
+			'do'	=> '404'
+		);
+	}
+	
+	/**
 	 * Dispatch action
 	 *
 	 * @param string
@@ -9,10 +28,37 @@ class Solidocs_Controller_Action extends Solidocs_Controller
 	public function dispatch_action($action){
 		$action = strtolower($action);
 		
-		if(method_exists($this,'do_'.$action)){
-			$action = 'do_'.$action;
+		if(isset($this->model->user) AND isset($this->acl[$action]) AND !$this->model->user->has_access($this->acl[$action]['level'])){
+			if($this->acl[$action]['do'] !== false){
+				$this->forward($this->acl[$action]['do']);
+			}
+			
+			return false;
+		}
+		
+		if(method_exists($this, 'do_' . $action)){
+			$action = 'do_' . $action;
+		}
+		else{
+			$action = 'do_404';
 		}
 		
 		$this->$action();
+	}
+	
+	/**
+	 * Forward
+	 *
+	 * @param string
+	 */
+	public function forward($action){
+		$this->dispatch_action($action);
+	}
+	
+	/**
+	 * 404
+	 */
+	public function do_404(){
+		$this->load->view('404');
 	}
 }
