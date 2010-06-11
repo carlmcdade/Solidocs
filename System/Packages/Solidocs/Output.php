d8a4d0d767f7574cfcec51817bef365a39a54340
Karl Roos <karlroos93@gmail.com>
2010-06-11 13:19:19 +0200

    Solidocs_Output::debug_box() has now been moved to Solidocs_Helper_Debug::bar(), you can now specifiy your debug helper with Solidocs_Output::$debug_helper and you render the helper by setting Solidocs_Output::$render_debug to true.

--- System/Packages/Solidocs/Output.php
+++ System/Packages/Solidocs/Output.php
@@ -14,9 +14,14 @@ class Solidocs_Output extends Solidocs_Base
 	);
 	
 	/** 
-	 * Debug box
+	 * Render debug
 	 */
-	public $debug_box = false;
+	public $render_debug = false;
+	
+	/**
+	 * Debug helper
+	 */
+	public $debug_helper = 'debug_bar';
 	
 	/**
 	 * Call magic method
@@ -102,8 +107,8 @@ class Solidocs_Output extends Solidocs_Base
 		
 		$output = ob_get_clean();
 		
-		if($this->debug_box){
-			$output .= $this->render_debug();
+		if($this->render_debug == true){
+			$output .= $this->{$this->debug_helper}();
 		}
 		
 		echo $output;
@@ -127,96 +132,5 @@ class Solidocs_Output extends Solidocs_Base
 		else{
 			echo $views;
 		}
-	}
-	
-	/**
-	 * Render debug
-	 *
-	 * @return string
-	 */
-	public function render_debug(){
-		$debug = array(
-			'General'	=> array(
-				'Time to generate'	=> microtime_since(STARTTIME),
-				'Memory usage'		=> round(memory_get_usage() / 1024 / 1024, 5) . ' MB',
-				'Included files'	=> count(get_included_files())
-			),
-			'Database queries'	=> debug($this->db->instance->queries, '', true),
-			'Errors'			=> debug($this->error->errors, '', true),
-			'ACL'				=> debug($this->acl->list, '', true),
-			'$_GET'				=> debug($_GET, '', true),
-			'$_POST'			=> debug($_POST, '', true),
-			'$_FILES'			=> debug($_FILES, '', true),
-			'$_SESSION'			=> debug($_SESSION, '', true),
-			'$_COOKIE'			=> debug($_COOKIE, '', true)
-		);
-
-		$output = '
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
-		<script type="text/javascript">
-		$(document).ready(function(){
-			
-			$("#debug_box ul li a").click(function(){
-				
-				$("#debug_box ul li div").hide();
-				$(this).siblings().show();
-				
-			});
-			
-		});
-		</script>
-		<style type="text/css">
-		#debug_box
-		{position: fixed; bottom: 0; left: 0; width: 100%; height: 25px; background: #444; border-top: 1px solid #333;}
-		
-		#debug_box ul
-		{margin-left: 10px;}
-		
-		#debug_box ul li
-		{display: inline; position: relative;}
-		
-		#debug_box ul li a
-		{display: block; float: left; height: 25px; line-height: 25px; padding: 0 10px; font-size: 10px; background: #ccc; color: #444; border: 1px solid #666; border-top: 0; border-bottom: 0; margin-right: 10px;}
-		
-		#debug_box ul li div
-		{position: absolute; bottom: 1px; left: 0; background: #ccc; border: 1px solid #666; border-bottom: 0; display: none;}
-		
-		#debug_box ul li div.item
-		{padding: 5px;}
-		
-		#debug_box ul li div table
-		{width: 500px;}
-		
-		#debug_box ul li div table tr td
-		{padding: 5px;}
-		
-		#debug_box ul li div table tr td:first-child
-		{font-weight: bold; padding-right: 20px;}
-		
-		#debug_box ul li div table tr:nth-child(even) td
-		{background: #ddd;}
-		</style>
-		<div id="debug_box"><ul>';
-
-		foreach($debug as $section => $parts){
-			$output .= '<li>';
-			
-			if(!is_array($parts)){
-				$output .= '<div class="item">' . $parts . '</div>';
-			}
-			else{
-				$output .= '<div><table>';
-				
-				foreach($parts as $title => $content){
-					$output .= '<tr><td>' . $title . '</td><td>' . $content . '</td></tr>';
-				}
-				
-				$output .= '</table></div>';
-			}
-			
-			$output .= '<a href="#">' . $section . '</a></li>';
-		}
-		
-		return $output . '<li><a href="#">X</a></ul></div>';
 	}
 }
