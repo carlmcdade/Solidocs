a89819db011060d1b51561a633e87ca79dcc002a
Karl Roos <karlroos93@gmail.com>
2010-06-12 18:18:52 +0200

    Solidocs_Controller_Content::do_index() now selects content only by uri and not by locale. If the user enters a page with a different locale then the locale will be set.

--- System/Packages/Solidcms/Controller/Content.php
+++ System/Packages/Solidcms/Controller/Content.php
@@ -19,15 +19,18 @@ class Solidcms_Controller_Content extends Solidocs_Controller_Action
 		}
 		
 		$content = $this->model->cms->get_content(array(
-			'uri'		=> $uri,
-			'locale'	=> $this->locale
-		));
+			'uri' => $uri
+		);
 		
 		if(count($content) == 0){
 			$this->load->view('404');
 		}
 		else{
 			$this->theme->title_parts[] = $content['title'];
+			
+			if(!empty($content['locale']) AND $content['locale'] !== $this->locale){
+				$this->i18n->set_locale($content['locale']);
+			}
 						
 			$this->load->view($content['view'], $content);
 		}
