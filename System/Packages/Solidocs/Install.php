17568ae599bd60625e81d33c96cca4728a2cbd55
Karl Roos <karlroos93@gmail.com>
2010-06-17 23:25:39 +0200

    Since Solidocs is MySQL-only for the time being there's no need to have an Solidocs_Install adapter. Merged Solidocs_Install_Mysql with Solidocs_Install.

--- System/Packages/Solidocs/Install.php
+++ System/Packages/Solidocs/Install.php
@@ -1,16 +1,6 @@
 <?php
 class Solidocs_Install extends Solidocs_Base
-{
-	/**
-	 * Instance
-	 */
-	public $instance;
-	
-	/**
-	 * Adapter
-	 */
-	public $adapter = 'Solidocs_Install_Mysql';
-	
+{	
 	/**
 	 * Tables
 	 */
@@ -22,25 +12,74 @@ class Solidocs_Install extends Solidocs_Base
 	public $data;
 	
 	/**
-	 * Init
+	 * Engine
 	 */
-	public function init(){
-		$this->instance			= new $this->adapter;
-		$this->instance->tables	= $this->tables;
-		$this->instance->data	= $this->data;
-	}
-	
+	public $engine = 'MyISAM';
+
 	/**
 	 * Install
 	 */
 	public function install(){
-		$this->instance->install();
+		foreach($this->tables as $table => $fields){
+			$sql		= 'CREATE TABLE IF NOT EXISTS `' . $table . '` (' . "\n";
+			$indexes	= array();
+			
+			foreach($fields as $name => $field){
+				$sql .= '`' . $name . '` ' . $this->types[$field['type']];
+				
+				if($field['type'] == 'bool'){
+					$field['length'] = 1;
+				}
+				
+				if(isset($field['length'])){
+					$sql .= '( ' . $field['length'] . ' )';
+				}
+				
+				$sql .= ' NOT NULL ';
+				
+				if(isset($field['auto_inc'])){
+					$sql .= 'AUTO_INCREMENT ';
+				}
+				
+				if(isset($field['primary'])){
+					$sql .= 'PRIMARY KEY ';
+				}
+				
+				if(isset($field['default'])){
+					$sql .= 'DEFAULT \'' . $field['default'] . '\'';
+				}
+				
+				if(isset($field['index'])){
+					$indexes[] = 'INDEX ( `' . $name . '` ) ';
+				}
+				
+				if(isset($field['unique'])){
+					$indexes[] = 'UNIQUE ( `' . $name . '` ) ';
+				}
+				
+				$sql .= ',' . "\n";
+			}
+			
+			if(count($indexes) !== 0){
+				$sql .= implode(',' . "\n", $indexes) . "\n";
+			}
+			
+			$this->db->sql(trim($sql, ',') . ') ENGINE=' . $this->engine . ';')->run();
+		}
+		
+		if(count($this->data) !== 0){
+			foreach($this->data as $table => $item){
+				$this->db->insert_into($table, $item)->run();
+			}
+		}
 	}
 	
 	/**
 	 * Uninstall
 	 */
 	public function uninstall(){
-		$this->instance->uninstall();
+		foreach($this->tables as $table => $fields){
+			$this->db->sql('DROP TABLE IF EXISTS `' . $table . '`;')->run();
+		}
 	}
 }
